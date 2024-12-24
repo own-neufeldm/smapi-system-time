6aff8b7b090f573ef7d3f9ce3060bc924f4d8dbc
Martin Neufeld <m.neufeld946@gmail.com>
2024-12-24 10:57:29 +0100

    feat: allow draw time to be toggled

--- SystemTime/ModEntry.cs
+++ SystemTime/ModEntry.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Reflection;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
@@ -6,6 +7,29 @@ using StardewValley;
 
 namespace SystemTime
 {
+  public static class DrawOns
+  {
+    public static readonly string Rendered = "Rendered";
+    public static readonly string RenderedActiveMenu = "Rendered active menu";
+    public static readonly string RenderedHud = "Rendered HUD";
+    public static readonly string RenderedStep = "Rendered step";
+    public static readonly string RenderedWorld = "Rendered world";
+
+    public static List<string> All()
+    {
+      List<string> all = new();
+
+      foreach (FieldInfo fieldInfo in typeof(DrawOns).GetFields())
+      {
+        string? value = (string?)fieldInfo.GetValue(null);
+        if (value is not null)
+          all.Add(value);
+      }
+
+      return all;
+    }
+  }
+
   internal sealed class ModEntry : Mod
   {
     private ModConfig? Config;
@@ -20,7 +44,12 @@ namespace SystemTime
       this.LabelFont = helper.GameContent.Load<SpriteFont>("Fonts/SmallFont");
       helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
       helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+
+      helper.Events.Display.Rendered += this.OnRendered;
+      helper.Events.Display.RenderedActiveMenu += this.OnRenderedActiveMenu;
       helper.Events.Display.RenderedHud += this.OnRenderedHud;
+      helper.Events.Display.RenderedStep += this.OnRenderedStep;
+      helper.Events.Display.RenderedWorld += this.OnRenderedWorld;
     }
 
     private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
@@ -42,6 +71,14 @@ namespace SystemTime
           getValue: () => this.Config.ToggleKeybind,
           setValue: value => this.Config.ToggleKeybind = value
       );
+      configMenu.AddTextOption(
+          mod: this.ModManifest,
+          name: () => "Draw on:",
+          tooltip: () => "When to draw the label in the rendering process.",
+          getValue: () => this.Config.DrawOn,
+          setValue: value => this.Config.DrawOn = value,
+          allowedValues: DrawOns.All().ToArray()
+      );
     }
 
     private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
@@ -54,7 +91,47 @@ namespace SystemTime
       this.Monitor.Log($"Toggled {state}.", LogLevel.Debug);
     }
 
+    private void OnRendered(object? sender, RenderedEventArgs e)
+    {
+      if (this.Config is null || !this.Config.DrawOn.Equals(DrawOns.Rendered))
+        return;
+
+      this.DrawLabel();
+    }
+
+    private void OnRenderedActiveMenu(object? sender, RenderedActiveMenuEventArgs e)
+    {
+      if (this.Config is null || !this.Config.DrawOn.Equals(DrawOns.RenderedActiveMenu))
+        return;
+
+      this.DrawLabel();
+    }
+
     private void OnRenderedHud(object? sender, RenderedHudEventArgs e)
+    {
+      if (this.Config is null || !this.Config.DrawOn.Equals(DrawOns.RenderedHud))
+        return;
+
+      this.DrawLabel();
+    }
+
+    private void OnRenderedStep(object? sender, RenderedStepEventArgs e)
+    {
+      if (this.Config is null || !this.Config.DrawOn.Equals(DrawOns.RenderedStep))
+        return;
+
+      this.DrawLabel();
+    }
+
+    private void OnRenderedWorld(object? sender, RenderedWorldEventArgs e)
+    {
+      if (this.Config is null || !this.Config.DrawOn.Equals(DrawOns.RenderedWorld))
+        return;
+
+      this.DrawLabel();
+    }
+
+    private void DrawLabel()
     {
       if (this.LabelFont is null || this.LabelTexture is null || !this.Draw)
         return;
